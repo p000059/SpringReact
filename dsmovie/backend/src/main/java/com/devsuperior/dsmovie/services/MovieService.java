fd2fa24e16fcb0900b49da0540191fcaa02a018f
p000059 <marcelo.acad@gmail.com>
2022-01-13 02:06:17 -0300

    20220113020600AM

--- dsmovie/backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
+++ dsmovie/backend/src/main/java/com/devsuperior/dsmovie/services/MovieService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.repositories.MovieRepository;
